7b9c5dff8fed219be6475e3f2191b89c69e94f64
Bitwise Mage <default@bitwisemage.com>
2025-04-24 03:41:13 +0100

    Early threading work

---
+++ src/core/thread.cpp
@@ -0,0 +1,40 @@
+﻿#include "thread.h"
+
+#include <SDL3/SDL_thread.h>
+
+#include "core/tracing.h"
+
+static int SdlThreadFunc(void* data)
+{
+    Thread* thread = static_cast<Thread*>(data);
+    // log thread starting
+    int exit_code = thread->Main();
+    // log thread exiting
+    return exit_code;
+}
+
+
+Thread::Thread(const char* thread_name)
+{
+    BITFORGE_TRACING;
+
+    SDL_Thread* thread = SDL_CreateThread(SdlThreadFunc, thread_name, this);
+    if (!thread)
+    {
+        return;
+    }
+
+    m_thread_created = true;
+    m_thread = thread;
+}
+
+bool Thread::Join() const
+{
+    if (!m_thread || !m_thread_created)
+    {
+        return false;
+    }
+
+    SDL_WaitThread(m_thread, nullptr);
+    return true;
+}
---
+++ src/core/thread.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+#include <cstdint>
+
+struct SDL_Thread;
+
+class Thread
+{
+public:
+    Thread() = delete;
+    explicit Thread(const char* thread_name);
+    virtual ~Thread() = default;
+
+    virtual int32_t Main() = 0;
+    bool Join() const;
+
+private:
+    SDL_Thread* m_thread = nullptr;
+    bool m_thread_created = false;
+};
